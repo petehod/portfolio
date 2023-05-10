cc886eaceed29ba81dd2c0592126ad94b6853d73
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-05-10 09:19:27 -0500

    projects page media queries added for mobile + tab

--- src/project-data/projects-data.js
+++ src/project-data/projects-data.js
@@ -75,77 +75,77 @@ export const projectsData = [
     projectImage: akemiImage,
     projectImageAlt: "The landing page for Akemi's website",
   },
-  // {
-  //   projectTitle: "Meloroids",
-  //   projectLogline: "Applying Pareto’s Principle to melody creation",
-  //   projectDescription:
-  //     "Meloroids is what I use on a daily basis to write melodies. It is a database populated with the most successful chord progressions in the genres I write in the most.",
-  //   projectShortDescription: "Melody creation simplified",
-  //   projectRepository: "https://github.com/petehodnefield/meloroids-typescript",
-  //   projectLive: "https://meloroids.io",
-  //   projectTechUsed: [
-  //     "MongoDB",
-  //     "GraphQL",
-  //     "Apollo Client",
-  //     "Node.js",
-  //     "Next.js",
-  //     "React.js",
-  //     "Tailwind CSS",
-  //   ],
-  //   projectImage: meloroidsImage,
-  //   projectImageAlt:
-  //     "The landing page for meloroids, which makes melody writing easier.",
-  // },
-  // {
-  //   projectTitle: "BeatGenius",
-  //   projectLogline:
-  //     "Custom music production from an exclusive producer collective.",
-  //   projectDescription:
-  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue, ex quis ultricies pulvinar, est eros tempor turpis, vel rhoncus magna diam eget mauris. Curabitur viverra nunc non justo congue, vitae sodales orci faucibus. Nam nec gravida diam, vitae efficitur odio. Pellentesque purus nisl, mollis ac felis nec, aliquet semper diam. Praesent at eros tortor.",
-  //   projectShortDescription: "Customized music production",
+  {
+    projectTitle: "Meloroids",
+    projectLogline: "Applying Pareto’s Principle to melody creation",
+    projectDescription:
+      "Meloroids is what I use on a daily basis to write melodies. It is a database populated with the most successful chord progressions in the genres I write in the most.",
+    projectShortDescription: "Melody creation simplified",
+    projectRepository: "https://github.com/petehodnefield/meloroids-typescript",
+    projectLive: "https://meloroids.io",
+    projectTechUsed: [
+      "MongoDB",
+      "GraphQL",
+      "Apollo Client",
+      "Node.js",
+      "Next.js",
+      "React.js",
+      "Tailwind CSS",
+    ],
+    projectImage: meloroidsImage,
+    projectImageAlt:
+      "The landing page for meloroids, which makes melody writing easier.",
+  },
+  {
+    projectTitle: "BeatGenius",
+    projectLogline:
+      "Custom music production from an exclusive producer collective.",
+    projectDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue, ex quis ultricies pulvinar, est eros tempor turpis, vel rhoncus magna diam eget mauris. Curabitur viverra nunc non justo congue, vitae sodales orci faucibus. Nam nec gravida diam, vitae efficitur odio. Pellentesque purus nisl, mollis ac felis nec, aliquet semper diam. Praesent at eros tortor.",
+    projectShortDescription: "Customized music production",
 
-  //   projectRepository: "https://github.com/petehodnefield/beat-genius",
-  //   projectLive: "https://beat-genius.vercel.app/",
-  //   projectTechUsed: ["React.js", "Next.js", "CSS"],
-  //   projectImage: beatGeniusImage,
-  //   projectImageAlt:
-  //     "The landing page for BeatGenius, which provides custom music production.",
-  // },
-  // {
-  //   projectTitle: "Brews with Brule",
-  //   projectLogline: "Social media for beer enthusiasts",
-  //   projectDescription:
-  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue, ex quis ultricies pulvinar, est eros tempor turpis, vel rhoncus magna diam eget mauris. Curabitur viverra nunc non justo congue, vitae sodales orci faucibus. Nam nec gravida diam, vitae efficitur odio. Pellentesque purus nisl, mollis ac felis nec, aliquet semper diam. Praesent at eros tortor.",
-  //   projectShortDescription: "Social media for beer drinkers",
+    projectRepository: "https://github.com/petehodnefield/beat-genius",
+    projectLive: "https://beat-genius.vercel.app/",
+    projectTechUsed: ["React.js", "Next.js", "CSS"],
+    projectImage: beatGeniusImage,
+    projectImageAlt:
+      "The landing page for BeatGenius, which provides custom music production.",
+  },
+  {
+    projectTitle: "Brews with Brule",
+    projectLogline: "Social media for beer enthusiasts",
+    projectDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue, ex quis ultricies pulvinar, est eros tempor turpis, vel rhoncus magna diam eget mauris. Curabitur viverra nunc non justo congue, vitae sodales orci faucibus. Nam nec gravida diam, vitae efficitur odio. Pellentesque purus nisl, mollis ac felis nec, aliquet semper diam. Praesent at eros tortor.",
+    projectShortDescription: "Social media for beer drinkers",
 
-  //   projectRepository: "https://github.com/petehodnefield/brews-with-brule",
-  //   projectLive: "https://bwb-frontend.vercel.app/",
-  //   projectTechUsed: [
-  //     "MongoDB",
-  //     "GraphQL",
-  //     "Apollo Client",
-  //     "Node.js",
-  //     "Next.js",
-  //     "React.js",
-  //     "Tailwind CSS",
-  //   ],
-  //   projectImage: BWBImage,
-  //   projectImageAlt:
-  //     "The landing page for Brews with Brule, which is a social media for beer enthusiasts.",
-  // },
-  // {
-  //   projectTitle: "Akemi",
-  //   projectLogline: "Landing page for French pop artist Akemi",
-  //   projectDescription:
-  //     " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue, ex quis ultricies pulvinar, est eros tempor turpis, vel rhoncus magna diam eget mauris. Curabitur viverra nunc non justo congue, vitae sodales orci faucibus. Nam nec gravida diam, vitae efficitur odio. Pellentesque purus nisl, mollis ac felis nec, aliquet semper diam. Praesent at eros tortor.",
-  //   projectShortDescription: "Artist site for a pop singer",
+    projectRepository: "https://github.com/petehodnefield/brews-with-brule",
+    projectLive: "https://bwb-frontend.vercel.app/",
+    projectTechUsed: [
+      "MongoDB",
+      "GraphQL",
+      "Apollo Client",
+      "Node.js",
+      "Next.js",
+      "React.js",
+      "Tailwind CSS",
+    ],
+    projectImage: BWBImage,
+    projectImageAlt:
+      "The landing page for Brews with Brule, which is a social media for beer enthusiasts.",
+  },
+  {
+    projectTitle: "Akemi",
+    projectLogline: "Landing page for French pop artist Akemi",
+    projectDescription:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue, ex quis ultricies pulvinar, est eros tempor turpis, vel rhoncus magna diam eget mauris. Curabitur viverra nunc non justo congue, vitae sodales orci faucibus. Nam nec gravida diam, vitae efficitur odio. Pellentesque purus nisl, mollis ac felis nec, aliquet semper diam. Praesent at eros tortor.",
+    projectShortDescription: "Artist site for a pop singer",
 
-  //   projectRepository: "https://github.com/petehodnefield/akemi",
-  //   projectLive: "https://akemi.onrender.com/",
-  //   projectTechUsed: ["React.js", "CSS"],
-  //   projectImage: akemiImage,
-  //   projectImageAlt: "The landing page for Akemi's website",
-  // },
+    projectRepository: "https://github.com/petehodnefield/akemi",
+    projectLive: "https://akemi.onrender.com/",
+    projectTechUsed: ["React.js", "CSS"],
+    projectImage: akemiImage,
+    projectImageAlt: "The landing page for Akemi's website",
+  },
 ];
 
 // {
