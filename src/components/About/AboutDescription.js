b0ae36a05afea5ce56f38b86d8afceb35fccfbfb
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-05-25 11:57:29 -0500

    changed resume link

--- src/components/About/AboutDescription.js
+++ src/components/About/AboutDescription.js
@@ -57,7 +57,7 @@ const AboutDescription = () => {
         </p>
         <Link
           className="btn--large btn--link rounded"
-          href="https://www.dropbox.com/s/pliol9uv4j1vwzl/Peter%20Hodnefield%27s%20Resume.pdf?dl=0"
+          href="https://www.dropbox.com/s/7so60w7lwd9ycwn/Peter%20Hodnefield%27s%20Resume.pdf?dl=0"
           target="__blank"
         >
           Resume
